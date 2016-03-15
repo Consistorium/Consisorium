aaa2b62379b80ef532be6ba57b1dadc94dd176db
Nicky <newmast@abv.bg>
2016-03-15 21:57:22 +0300

    Added seperate game engine project

--- Consistorium/Game/Menu/MouseButtonEnum.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-enum class MouseButton
-{
-	Left = 1,
-	Right = 2
-};
--- Consistorium/Interfaces/IGraphicsRenderer.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class IGraphicsRenderer
-{
-public:
-	virtual ~IGraphicsRenderer() { }
-
-	virtual void DrawDescriptions() = 0;
-};
